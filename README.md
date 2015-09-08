47502a887787d22464edc69f16ebc963f0f90763
Falicon <info@falicon.com>
2015-09-08 08:40:12 -0600

    conversationlist build email list

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # conversationlist
-Open source version of email address collection script
+Open source version of email address collection script. 
+
+## Requirements & installation
+
+- A context.io 2.0 api account ( https://context.io/ )
+- The contextio python library ( pip install contextio )
+- The python csv library ( pip install csv )
+
+## Useage
+
+- Edit the build_email_list.py script configuration section to contain your credentials
+- Run python build_email_list.py (if all goes well it will report as it builds the list and the final result will be the creation of a emails.csv file)
+
+### After completion
+
+This script simply builds a csv file of any email address you've interacted with over the years (and tells you how many emails you've exchanged with the given address).
+
+You will most likely want to scrub it for any known bad addresses or email lists that may slip through and de-dup it for those who use multiple email addresses when interacting with you.
+
+#### Note
+
+This script relies on the contextio api and can only collect email addresses from the emails that you have granted them access to and that they've then been able to index. It does not represent a complete history of your email activity (i.e. it won't include things you've deleted or most likely anything from a spam folder).
---
+++ build_email_list.py
@@ -0,0 +1,83 @@
+import contextio as c
+import csv
+
+############################################
+### Script configuration (customize to your details)
+############################################
+CONSUMER_KEY = 'YOUR_CONTEXTIO_CONSUMER_KEY'
+CONSUMER_SECRET = 'YOUR_CONTEXTIO_CONSUMER_SECRET'
+CONTEXTIO_AUTHED_EMAIL = 'THE EMAIL YOU WANT TO BUILD YOUR LIST FROM'
+# ignore email addresses that contain any of the following words
+ignores = [
+  'buzz+','help','notify','info',
+  'admin','no-reply','noreply',
+  'service','notifications'
+]
+
+############################################
+### Script 
+############################################
+context_io = c.ContextIO(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+
+emails = {}
+accounts = context_io.get_accounts(email=CONTEXTIO_AUTHED_EMAIL)
+for account in accounts:
+  has_more = True
+  offset = 0
+  page = 0
+  while has_more:
+    page += 1
+    messages = account.get_messages(limit=100, offset=offset)
+    added = 0
+    for message in messages:
+      try:
+        tos = message.addresses['to']
+        for to_user in tos:
+          email = to_user['email']
+          skip = False
+          for ignore in ignores:
+            if email.find(ignore) > -1:
+              skip = True
+          if not skip:
+            if email not in emails.keys():
+              added += 1
+              try:
+                emails[email] = {'name':to_user['name'],'count':1}
+              except:
+                emails[email] = {'name':'','count':1}
+            else:
+              emails[email]['count'] += 1
+
+        from_user = message.addresses['from']
+        email = from_user['email']
+        skip = False
+        for ignore in ignores:
+          if email.find(ignore) > -1:
+            skip = True
+        if not skip:
+          if email not in emails.keys():
+            added += 1
+            try:
+              emails[email] = {'name':from_user['name'],'count':1}
+            except:
+              emails[email] = {'name':'','count':1}
+          else:
+            emails[email]['count'] += 1
+      except:
+        pass
+    print "page %s added %s email address" % (page, added)
+    offset += 100
+    if len(messages) < 100:
+      has_more = False
+
+# sort the emails by count and write to a csv file
+with open('emails.csv', 'wb') as csvfile:
+  fieldnames = ['EMAIL', 'NAME', 'COUNT']
+  emailwriter = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+  emailwriter.writerow(fieldnames)
+  for email in sorted(emails.iteritems(), key=lambda (x, y): y['count']):
+    try:
+      emailwriter.writerow([email[0], email[1]['name'], email[1]['count']])
+    except:
+      pass
+
